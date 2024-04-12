0faeaeb17ec37c332e350997310dc377f30245e1
RMohanapriya <115524999+RMohanapriya@users.noreply.github.com>
2024-04-12 23:55:58 +0530

    Add files via upload

---
+++ task2/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const taskList = document.getElementById("task-list");
+    const taskInput = document.getElementById("task-input");
+    const deadlineInput = document.getElementById("deadline-input");
+    const prioritySelect = document.getElementById("priority-select");
+    const dependencySelect = document.getElementById("dependency-select");
+    const addBtn = document
+  
